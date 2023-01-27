e6a8396b113a82912f919b2ea2c46bcb28f6e70b
Mauro Alderete <alderete.mauro@gmail.com>
2023-01-27 19:52:28 -0300

    feat: add coverage rules

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,5 +11,22 @@ export default defineConfig({
       '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
     ],
     environment: 'happy-dom',
+    coverage: {
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/*.cy.{js,jsx,ts,tsx}',
+        '**/*.d.{js,jsx,ts,tsx}',
+        '**/main.ts',
+      ],
+      all: true,
+      branches: 80,
+      lines: 80,
+      functions: 80,
+      statements: 80,
+    },
   },
 });
