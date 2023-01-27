ab4b157854c3496041d48156bdcd5da828db3e67
Mauro Alderete <alderete.mauro@gmail.com>
2023-01-27 18:55:28 -0300

    fix: resolve lint errors and ignore vitest.config.ts lint comprobation

--- src/counter.spec.ts
+++ src/counter.spec.ts
@@ -3,9 +3,7 @@ import { setupCounter } from './counter';
 
 describe('Counter', () => {
   it('should initialize a counter button', () => {
-    let button: HTMLButtonElement;
-
-    button = <HTMLButtonElement>document.createElement('button');
+    const button: HTMLButtonElement = <HTMLButtonElement>document.createElement('button');
 
     setupCounter(button);
 
@@ -13,9 +11,7 @@ describe('Counter', () => {
   });
 
   it('should increase the counter', () => {
-    let button: HTMLButtonElement;
-
-    button = <HTMLButtonElement>document.createElement('button');
+    const button: HTMLButtonElement = <HTMLButtonElement>document.createElement('button');
 
     setupCounter(button);
 
