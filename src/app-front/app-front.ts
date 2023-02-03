9857196bd9c195306e1ad715d0f861a15bc5f6f4
Mauro Alderete <alderete.mauro@gmail.com>
2023-02-03 17:02:37 -0300

    fix: remove import crashed

--- src/app-front/app-front.ts
+++ src/app-front/app-front.ts
@@ -1,6 +1,5 @@
 import { LitElement, html, unsafeCSS, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { AppModal } from '../app-modal/app-modal';
 
 /*eslint-disable */
 import style from './app-front.css?inline' assert { type: 'css' };
